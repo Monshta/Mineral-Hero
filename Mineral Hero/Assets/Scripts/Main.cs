0d47367723f71b4024628cd5a36a977912d2dbe8
Mohammed Azad <varsity.azad@gmail.com>
2015-02-21 22:50:09 -0800

    v0.4
    Added all the costs

--- Mineral Hero/Assets/Scripts/Main.cs
+++ Mineral Hero/Assets/Scripts/Main.cs
@@ -10,18 +10,25 @@ public class Main : MonoBehaviour {
 	public int clickStrength = 1;	//HOW MANY MINERALS YOU GAIN PER CLICK
 
 	public int clickerCount = 0;
+	public int clickerCost = 5;
 	public Text clickerCountUI;
 	public int minerCount = 0;
+	public int minerCost = 10;
 	public Text minerCountUI;
 	public int cartCount = 0;
+	public int cartCost = 50;
 	public Text cartCountUI;
 	public int robotCount = 0;
+	public int robotCost = 100;
 	public Text robotCountUI; // idk how to make the little images but i still made these for you
 	public int extractorCount = 0;
+	public int extractorCost = 300;
 	public Text extractorCountUI;
 	public int laserCount = 0;
+	public int laserCost = 700;
 	public Text laserCountUI;
 	public int goatCount = 0;
+	public int goatCost = 1000;
 	public Text goatCountUI;
 
 	void Awake (){
@@ -36,34 +43,62 @@ public class Main : MonoBehaviour {
 		cartCountUI.text = cartCount + "";
 	}
 	public void upgradeClick(int c) {	//UPGRADE CLICKER
-		clickStrength++;
-		clickerCount++;
-
+		if(mineralCount >= clickerCost){
+			clickStrength++;
+			clickerCount++;
+			mineralCount -= clickerCost;
+			clickerCost+=2;
+		}
+		else ;
 	}
 	public void upgradeMiner(int m){	//UPGRADE MINER
-		mineralPerSecond++;
-		minerCount++;
+		if (mineralCount >= minerCost) {
+			mineralPerSecond++;
+			minerCount++;
+			mineralCount -= minerCost;
+			minerCost += 3;
+	} else ;					
 	}
 	public void upgradeCart(int ca){	//UPGRADE CART
-		mineralPerSecond += 2;
-		cartCount++;
-	}
+			if (mineralCount >= cartCost) {
+				mineralPerSecond += 2;
+				cartCount++;
+				mineralCount -= cartCost;
+				cartCost += 10;
+			} else ;
+		}
 	public void upgradeRobot(int r){ //Robot upgrade
-		mineralPerSecond += 5;
-		robotCount ++;
+		if (mineralCount >= robotCost) {
+			mineralPerSecond += 5;
+			robotCount ++;
+			mineralCount -= robotCost;
+			robotCost += 25;
+			} else ;
 	}
 	public void upgradeExtractor(int e){ //extractor upgrade
-		clickStrength += 5;
-		extractorCount ++;
+		if (mineralCount >= extractorCost) {
+			clickStrength += 5;
+			extractorCount ++;
+			mineralCount-= extractorCost;
+			extractorCost+= 50;
+			} else ;
 	}
 	public void upgradeLaser(int l){ //Laser upgrade
-		mineralPerSecond += 10;
-		laserCount ++;
+		if(mineralCount>= laserCost){
+			mineralPerSecond += 10;
+			laserCount ++;
+			mineralCount -= laserCost;
+			laserCost+=100;
+		}else ;
 	}
-	public void upgradeGoat(int g){ //goat upgrade
+	public void upgradeGoat(int g){ //goat upgrade, yep he's OP
+		if(mineralCount>= goatCost){
 		mineralPerSecond += 10;
 		clickStrength += 10;
 		goatCount ++;
+		mineralCount -= goatCost;
+		goatCost+=500;
+		}else ;
 
 	}
 }
