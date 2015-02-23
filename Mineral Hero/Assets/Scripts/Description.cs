bb088f89134c3ad6c8936d36bb6fcf8ff54acdb4
Monshta <frgimena@gmail.com>
2015-02-22 23:40:41 -0800

    v0.6
    -Description UI

---
+++ Mineral Hero/Assets/Scripts/Description.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Description : MonoBehaviour {
+
+	Text descText;
+	private GameObject gameManager;
+	private Main main;
+
+	void Start () {
+		descText = GetComponent<Text> ();
+		gameManager = GameObject.Find ("GameManager");
+		main = gameManager.GetComponent<Main> ();
+	}	
+
+	public void ClickerMessage (string c) {
+						descText.text = "Clicker" +
+						"\nCost: " + main.clickerCost + 
+						"\nGaining: " + main.clickStrength + " Per Click" +
+						"\nClick Click Click Click Click Click Click Click Click.";
+	}
+
+	public void MinerMessage (string m) {
+		descText.text = "Miner" +
+						"\nCost: " + main.minerCost + 
+						"\nGaining: +" + main.minermps + " mps" +
+						"\nMiners Who Mine In Search Of The One True Goat.";
+	}
+
+	public void CartMessage (string ca) {
+		descText.text = "Mine Cart" +
+						"\nCost: " + main.cartCost + 
+						"\nGaining: +" + main.cartmps + " mps" +
+						"\nThe Carts Of The Miners.";
+	}
+
+	public void RobotMessage (string r) {
+						descText.text = "Robot" +
+						"\nCost: " + main.robotCost + 
+						"\nGaining: +" + main.robotmps + " mps" +
+						"\nBoom Bam Bop. Bada Bop Boom Pow!";
+	}
+
+	public void ExtractorMessage (string e) {
+						descText.text = "Extractor" +
+						"\nCost: " + main.extractorCost + 
+						"\nGaining: +" + main.extractormps + " mps" +
+						"\nI don't know what to put here yo.";
+	}
+
+	public void LaserMessage (string l) {
+						descText.text = "Laser" +
+						"\nCost: " + main.laserCost + 
+						"\nGaining: +" + main.lasermps + " mps" +
+						"\nLmfao it's 11:29PM and I'm tired.";
+	}
+
+	public void GoatMessage (string g) {
+						descText.text = "Goat" +
+						"\nCost: " + main.goatCost + 
+						"\nGaining: +" + main.goatmps + " mps" +
+						"\nIs that Gosu?!";
+	}
+}
+
+
+
+
+
+
+
--- Mineral Hero/Assets/Scripts/Main.cs
+++ Mineral Hero/Assets/Scripts/Main.cs
@@ -8,28 +8,39 @@ public class Main : MonoBehaviour {
 	public float mineralPerSecond = 0;	//HOW MANY MINERALS YOU GAIN PER SECOND
 	public float timer;
 
-	public int clickStrength = 1;	//HOW MANY MINERALS YOU GAIN PER CLICK
-
+	public int clickStrength = 1;
 	public int clickerCount = 0;
 	public int clickerCost = 5;
 	public Text clickerCountUI;
+
 	public int minerCount = 0;
 	public int minerCost = 10;
+	public int minermps = 1;
 	public Text minerCountUI;
+
 	public int cartCount = 0;
 	public int cartCost = 50;
+	public int cartmps = 2;
 	public Text cartCountUI;
+
 	public int robotCount = 0;
 	public int robotCost = 100;
-	public Text robotCountUI; // idk how to make the little images but i still made these for you
+	public int robotmps = 5;
+	public Text robotCountUI;
+
 	public int extractorCount = 0;
 	public int extractorCost = 300;
+	public int extractormps = 8;
 	public Text extractorCountUI;
+
 	public int laserCount = 0;
 	public int laserCost = 700;
+	public int lasermps = 10;
 	public Text laserCountUI;
+
 	public int goatCount = 0;
 	public int goatCost = 1000;
+	public int goatmps = 15;
 	public Text goatCountUI;
 
 	void Awake (){
@@ -64,7 +75,7 @@ public class Main : MonoBehaviour {
 	}
 	public void upgradeMiner(int m){	//UPGRADE MINER
 		if (mineralCount >= minerCost) {
-			mineralPerSecond++;
+			mineralPerSecond += minermps;
 			minerCount++;
 			mineralCount -= minerCost;
 			minerCost += 3;
@@ -72,7 +83,7 @@ public class Main : MonoBehaviour {
 	}
 	public void upgradeCart(int ca){	//UPGRADE CART
 		if (mineralCount >= cartCost) {
-			mineralPerSecond += 2;
+			mineralPerSecond += cartmps;
 			cartCount++;
 			mineralCount -= cartCost;
 			cartCost += 10;
@@ -80,7 +91,7 @@ public class Main : MonoBehaviour {
 	}
 	public void upgradeRobot(int r){ //Robot upgrade
 		if (mineralCount >= robotCost) {
-			mineralPerSecond += 5;
+			mineralPerSecond += robotmps;
 			robotCount ++;
 			mineralCount -= robotCost;
 			robotCost += 25;
@@ -88,7 +99,7 @@ public class Main : MonoBehaviour {
 	}
 	public void upgradeExtractor(int e){ //extractor upgrade
 		if (mineralCount >= extractorCost) {
-			mineralPerSecond += 8;
+			mineralPerSecond += extractormps;
 			extractorCount ++;
 			mineralCount-= extractorCost;
 			extractorCost+= 50;
@@ -96,7 +107,7 @@ public class Main : MonoBehaviour {
 	}
 	public void upgradeLaser(int l){ //Laser upgrade
 		if(mineralCount>= laserCost){
-			mineralPerSecond += 10;
+			mineralPerSecond += lasermps;
 			laserCount ++;
 			mineralCount -= laserCost;
 			laserCost+=100;
@@ -104,7 +115,7 @@ public class Main : MonoBehaviour {
 	}
 	public void upgradeGoat(int g){ //goat upgrade, yep he's OP
 		if(mineralCount>= goatCost){
-			mineralPerSecond += 15;
+			mineralPerSecond += goatmps;
 			goatCount ++;
 			mineralCount -= goatCost;
 			goatCost+=500;
