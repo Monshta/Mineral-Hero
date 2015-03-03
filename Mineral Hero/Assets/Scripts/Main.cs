0b01be9682e32658de7b6f4c2cca011acbc4575c
Mohammed Azad <varsity.azad@gmail.com>
2015-03-02 18:22:44 -0800

    v1.1
    added costs, but couldnt update because the costs werent updating in the
    game like they were in the script.

--- Mineral Hero/Assets/Scripts/Main.cs
+++ Mineral Hero/Assets/Scripts/Main.cs
@@ -7,56 +7,64 @@ public class Main : MonoBehaviour {
 	public float mineralCount = 0;	//TOTAL MINERALS
 	public float mineralPerSecond = 0;	//HOW MANY MINERALS YOU GAIN PER SECOND
 	public float timer;
-
+	
 	public int clickStrength = 1;
 	public int clickerCount = 0;
-	public int clickerCost = 5;
+	public int clickerCost = 125;
 	public Text clickerCountUI;
-
+	
 	public int minerCount = 0;
-	public int minerCost = 10;
+	public int minerCost = 25;
 	public int minermps = 1;
 	public Text minerCountUI;
-
+	
 	public int cartCount = 0;
 	public int cartCost = 50;
 	public int cartmps = 2;
 	public Text cartCountUI;
-
+	
 	public int robotCount = 0;
 	public int robotCost = 100;
 	public int robotmps = 5;
 	public Text robotCountUI;
-
+	
 	public int extractorCount = 0;
 	public int extractorCost = 300;
 	public int extractormps = 8;
 	public Text extractorCountUI;
-
+	
 	public int laserCount = 0;
 	public int laserCost = 700;
 	public int lasermps = 10;
 	public Text laserCountUI;
-
+	
 	public int goatCount = 0;
 	public int goatCost = 1000;
 	public int goatmps = 15;
 	public Text goatCountUI;
-
+	
 	public GameObject steroidModUI;
 	public GameObject oilModUI;
 	public GameObject batteryModUI;
 	public GameObject titaniumModUI;
 	public GameObject glassModUI;
 	public GameObject goatModUI;
-
+	
+	public int steroidModCost = 50;
+	public int oilModCost = 50;
+	public int batteryModCost = 50;
+	public int titaniumModCost = 50;
+	public int glassModCost = 50;
+	public int goatModCost = 50;
+	
+	
 	void Update (){
 		mineralPerSecond =  (minermps * minerCount)+
-							(cartmps * cartCount)+
-							(robotmps * robotCount)+
-							(extractormps * extractorCount)+
-							(lasermps * laserCount)+
-							(goatmps * goatCount);
+			(cartmps * cartCount)+
+				(robotmps * robotCount)+
+				(extractormps * extractorCount)+
+				(lasermps * laserCount)+
+				(goatmps * goatCount);
 		if (mineralPerSecond > 0) {
 			timer += Time.deltaTime;
 		}
@@ -78,14 +86,14 @@ public class Main : MonoBehaviour {
 			clickStrength++;
 			clickerCount++;
 			mineralCount -= clickerCost;
-			clickerCost+=2;
+			clickerCost*=2;
 		}
 	}
 	public void upgradeMiner(int m){	//UPGRADE MINER
 		if (mineralCount >= minerCost) {
 			minerCount++;
 			mineralCount -= minerCost;
-			minerCost += 3;
+			minerCost *= 2;
 		}				
 	}
 	public void upgradeCart(int ca){	//UPGRADE CART
@@ -109,7 +117,7 @@ public class Main : MonoBehaviour {
 			extractorCount ++;
 			mineralCount -= extractorCost;
 			extractorCost += 50;
-			}
+		}
 	}
 	public void upgradeLaser(int l){ //Laser upgrade
 		if(mineralCount>= laserCost){
@@ -127,36 +135,44 @@ public class Main : MonoBehaviour {
 			goatCost+=500;
 		}
 	}
-
+	
 	public void steroidMod(int s) {
-		minermps *= 2;
-		steroidModUI.SetActive (false);
+		if (mineralCount >= steroidModCost) {
+			minermps *= 2;
+			mineralCount-=steroidModCost;
+			steroidModUI.SetActive (false);
+		}
 	}
 	public void oilMod(int o) {
-		cartmps *= 2;
-		oilModUI.SetActive (false);
+		if (mineralCount >= oilModCost) {
+			cartmps *= 2;
+			mineralCount-=oilModCost;
+			oilModUI.SetActive (false);
+		}
 	}
 	public void batteryMod(int b) {
-		robotmps *= 2;
-		batteryModUI.SetActive (false);
+		if(mineralCount>=batteryModCost){
+			robotmps *= 2;
+			mineralCount-=batteryModCost;
+			batteryModUI.SetActive (false);
+		}
 	}
 	public void titaniumMod(int t) {
 		extractormps *= 2;
 		titaniumModUI.SetActive (false);
 	}
 	public void glassMod(int g) {
-		lasermps *= 2;
-		glassModUI.SetActive (false);
+		if(mineralCount>=glassModCost){	
+			lasermps *= 2;
+			mineralCount-=glassModCost;
+			glassModUI.SetActive (false);
+		}
 	}
 	public void goatMod(int go) {
-		goatmps *= 2;
-		goatModUI.SetActive (false);
+		if(mineralCount>=goatModCost){
+			goatmps *= 2;
+			mineralCount-=goatModCost;
+			goatModUI.SetActive (false);
+		}
 	}
 }
-
-
-
-
-
-
-
