3b9d18b67695c735aeb2daf8c273d5012c38ccb9
Mohammed Azad <varsity.azad@gmail.com>
2015-03-19 13:56:12 -0700

    v2.3
    mainMenu added

---
+++ Mineral Hero/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+
+
+public class MainMenu: MonoBehaviour
+{
+	public void startButton(int sb)
+	{
+		Application.LoadLevel("main");
+	}
+}
