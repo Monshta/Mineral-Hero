8e66f7673a7c1e9dcc143039f9328e7c4d443815
Monshta <frgimena@gmail.com>
2015-03-18 21:37:22 -0700

    v2.2
    -Miner Animations

---
+++ Mineral Hero/Assets/Scripts/MinerAnim.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinerAnim : MonoBehaviour {
+
+	private GameObject gameManager;
+	private Main main;
+	private SpriteRenderer spriteRend;
+
+	public int numberOfMiner;
+
+	void Start () {
+		gameManager = GameObject.Find ("GameManager");
+		main = gameManager.GetComponent<Main> ();
+		spriteRend = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	void Update () {
+		if (main.minerCount >= numberOfMiner) {
+			spriteRend.enabled = true;
+		}else {
+			spriteRend.enabled = false;
+		}
+	}
+}
